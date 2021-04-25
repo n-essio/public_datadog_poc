82a012f198a6ef9ec548a6982d9cafe010981f4b
fiorenzo pizza <fiorenzino@gmail.com>
2021-04-25 11:23:22 +0200

    first commit

---
+++ src/main/java/it/ness/poc/GreetingResource.java
@@ -0,0 +1,16 @@
+package it.ness.poc;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/public_datadog_poc")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
---
+++ src/test/java/it/ness/poc/GreetingResourceTest.java
@@ -0,0 +1,21 @@
+package it.ness.poc;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/public_datadog_poc")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
